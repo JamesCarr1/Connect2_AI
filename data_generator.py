Continued testing and fixed an issure where CrossEntropyLoss() was being given (softmax(logits), targets), rather than (logits, targets), i.e the logits did not need to be softmaxed before inputting.

data_generator.py
@@ -93,6 +93,7 @@ class Node:
         weights = scipy.stats.norm(maximum_score, 1).pdf(scores) # find weights from normal curve. COULD ADJUST STD DEV???
 
         return random.choices(list(self.children.items()), weights)[0] # randomly choose based on weights.
+        
 
     def select_action(self, choose_type):
         """
@@ -270,11 +271,12 @@ def normalised_ucb_score(parent: Node, child: Node, Z):
     """Calculates 'ucb_score', then normalises it between 0 """
 
 if __name__ == '__main__':
-    model = model_builder.LinearModelV0(input_shape=4, hidden_units=2, output_shape=1)
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    model = model_builder.LinearModelV0(input_shape=4, hidden_units=2, output_shape=1).to(device)
     game_generator = GameGenerator(model=model, game_type=connect2.Connect2Game)
 
-    num_games = 1000
-    num_sims = 15
+    num_games = 50
+    num_sims = 10
 
     start = time.time()
     time_taken = game_generator.generate_n_games(num_games=num_games, num_simulations=num_sims)
@@ -290,5 +292,5 @@ if __name__ == '__main__':
 
     df2 = pd.read_pickle(Path(os.getcwd()) / "generated_games" / f"LinearModelV0.1_{num_games}_games_{num_sims}_MCTS_sims.pkl")
 
-    print(df2.head(5))
+    print(df2.head(20))
     

engine.py
@@ -9,7 +9,8 @@ def train_step(model: torch.nn.Module,
                dataloader: torch.utils.data.DataLoader,
                value_loss_fn: torch.nn.Module,
                prior_loss_fn: torch.nn.Module,
-               optimizer: torch.optim.Optimizer,
+               value_optimizer: torch.optim.Optimizer,
+               prior_optimizer: torch.optim.Optimizer,
                device: torch.device,
                accuracy_fn) -> Tuple[float, float]:
     """
@@ -44,29 +45,28 @@ def train_step(model: torch.nn.Module,
         actions_logits, value_logits = model(board_state) # for now, just going to ignore softmaxing logits
 
         value_pred = torch.tanh(value_logits)
-        prior_pred = utils.softmax_mask_mean(actions_logits=actions_logits,
-                                            legal_moves_mask=legal_moves_mask)
+        prior_pred = torch.softmax(actions_logits, dim=1)
 
         ### Calculate loss
 
         # Loss functions
         vloss = value_loss_fn(value_pred, winner)
-        ploss = prior_loss_fn(prior_pred, expanded_target_priors)
-        loss = value_loss_fn(value_pred, winner) + prior_loss_fn(prior_pred, expanded_target_priors)
+        ploss = prior_loss_fn(actions_logits, expanded_target_priors) # CrossEntropyLoss is between LOGITS and target
+        loss = value_loss_fn(value_pred, winner) + prior_loss_fn(actions_logits, expanded_target_priors)
 
         # Loss values
         value_loss += vloss.item()
         prior_loss += ploss.item()
         train_loss += loss.item()
 
-        # Zero grad
-        optimizer.zero_grad()
+        # Zero grad, loss backward and step
+        value_optimizer.zero_grad()
+        vloss.backward(retain_graph=True)
+        value_optimizer.step()
 
-        # Loss backward
-        loss.backward()
-
-        # Optimizer step
-        optimizer.step()
+        prior_optimizer.zero_grad()
+        ploss.backward()
+        prior_optimizer.step()
 
         # Calculate accuracy metrics
         acc += accuracy_fn(value_logits, winner)
@@ -119,15 +119,14 @@ def test_step(model: torch.nn.Module,
 
             # Convert logits to predictions
             value_pred = torch.tanh(value_logits)
-            prior_pred = utils.softmax_mask_mean(actions_logits=actions_logits,
-                                                 legal_moves_mask=legal_moves_mask)
+            prior_pred = torch.softmax(actions_logits, dim=1)
 
             ### Calculate loss
 
             # Loss functions
             vloss = value_loss_fn(value_pred, winner)
-            ploss = prior_loss_fn(prior_pred, expanded_target_priors)
-            loss = value_loss_fn(value_pred, winner) + prior_loss_fn(prior_pred, expanded_target_priors)
+            ploss = prior_loss_fn(actions_logits, expanded_target_priors) # CrossEntropyLoss is between LOGITS and target
+            loss = value_loss_fn(value_pred, winner) + prior_loss_fn(actions_logits, expanded_target_priors)
 
             # Loss values
             value_loss += vloss.item()
@@ -148,7 +147,8 @@ def test_step(model: torch.nn.Module,
 def train(model: torch.nn.Module,
           train_dataloader: torch.utils.data.DataLoader,
           test_dataloader: torch.utils.data.DataLoader,
-          optimizer: torch.optim.Optimizer,
+          prior_optimizer: torch.optim.Optimizer,
+          value_optimizer: torch.optim.Optimizer,
           value_loss_fn: torch.nn.Module,
           prior_loss_fn: torch.nn.Module,
           epochs: int,
@@ -192,7 +192,8 @@ def train(model: torch.nn.Module,
                                                                                     dataloader=train_dataloader,
                                                                                     value_loss_fn=value_loss_fn,
                                                                                     prior_loss_fn=prior_loss_fn,
-                                                                                    optimizer=optimizer,
+                                                                                    value_optimizer=value_optimizer,
+                                                                                    prior_optimizer=prior_optimizer,
                                                                                     device=device,
                                                                                     accuracy_fn=accuracy_fn)
         

main.py
@@ -19,9 +19,10 @@ def generate_and_train(model,
                        epochs,
                        value_loss_fn,
                        prior_loss_fn,
-                       optimizer,
+                       value_optimizer,
+                       prior_optimizer,
                        device,
-                       scheduler=None):
+                       schedulers=None):
     """
     Generates a dataset, then trains from it. Saves the updated model and then repeats for num_gens.
     """
@@ -41,7 +42,8 @@ def generate_and_train(model,
         results = engine.train(model=model,
                     train_dataloader=train_dataloader,
                     test_dataloader=test_dataloader,
-                    optimizer=optimizer,
+                    value_optimizer=value_optimizer,
+                    prior_optimizer=prior_optimizer,
                     value_loss_fn=value_loss_fn,
                     prior_loss_fn=prior_loss_fn,
                     epochs=epochs,
@@ -53,10 +55,16 @@ def generate_and_train(model,
         save_path = Path(os.getcwd()) / "models" / f"{model}.{model.gen}.pt"
         torch.save(model.state_dict(), save_path)
 
-        if scheduler is not None:
-            scheduler.step()
+        if schedulers is not None:
+            for scheduler in schedulers:
+                scheduler.step()
 
+    return results
 
+def qt(x, i):
+    a = x[i].tolist()
+
+    return [round(y, 4) for y in a]
 
 if __name__ == '__main__':
     # Setup device-agnostic code
@@ -64,7 +72,7 @@ if __name__ == '__main__':
 
     # Setup model
     input_shape = 4
-    hidden_units = 2
+    hidden_units = 16
     output_shape = 1
     model = model_builder.LinearModelV0(input_shape=input_shape,
                                         hidden_units=hidden_units,
@@ -74,19 +82,44 @@ if __name__ == '__main__':
     value_loss_fn = torch.nn.MSELoss()
     prior_loss_fn = torch.nn.CrossEntropyLoss()
 
-    optimizer = torch.optim.Adam(params=model.parameters(),
-                                 lr=0.05)
-    scheduler = lr_scheduler.ExponentialLR(optimizer, gamma=0.8)
+    value_optimizer = torch.optim.Adam(params=model.parameters(),
+                                 lr=0.01)
+    prior_optimizer = torch.optim.Adam(params=model.parameters(),
+                                       lr=0.1)
     
-    generate_and_train(model=model,
+    value_scheduler = lr_scheduler.ExponentialLR(value_optimizer, gamma=0.99)
+    prior_scheduler = lr_scheduler.ExponentialLR(prior_optimizer, gamma=0.99)
+
+    
+    results = generate_and_train(model=model,
                        num_games=1000,
                        num_sims=10,
                        num_gens=10,
-                       epochs=4,
+                       epochs=6,
                        value_loss_fn=value_loss_fn,
                        prior_loss_fn=prior_loss_fn,
-                       optimizer=optimizer,
-                       device=device)
+                       value_optimizer=value_optimizer,
+                       prior_optimizer=prior_optimizer,
+                       device=device,
+                       schedulers=(value_scheduler, prior_scheduler))
+    
+    test_vector = torch.tensor([[1, 0, 0, -1],
+                                [-1, 0, 0, 0],
+                                [0, 0, 1, -1]], dtype=torch.float32).to(device)
+    target_priors = torch.tensor([[0, 0.72979, 0.27020, 0],
+                                  [0, 0.41657, 0.25619, 0.327],
+                                  [0.26969, 0.73030, 0, 0]])
+    target_values = torch.tensor([0, -1, 1])
+    
+    action_logits, value_logits = model(test_vector)
+
+    pred_priors = torch.softmax(action_logits, dim=1)
+    pred_values = torch.tanh(value_logits)
+
+    for i, board_state in enumerate(test_vector):
+        print(f"{board_state} | Priors: {qt(target_priors, i)} vs. {qt(pred_priors, i)} | Values: {round(target_values[i].item(), 4)} vs. {round(pred_values[i].item(), 4)}")
+        #print(f"{board_state} | Priors: {qt(target_priors, i)} vs. {qt(pred_priors, i)} / {qt(action_logits, i)} | Values: {round(target_values[i].item(), 4)} vs. {round(pred_values[i].item(), 4)}")
+
     
     """
     # Setup dataloaders
@@ -105,7 +138,7 @@ if __name__ == '__main__':
                  epochs=epochs,
                  device=device,
                  accuracy_fn=utils.value_acc)
-
+    """
     results_to_plot = [("value_train_loss", 'r', '-'),
                        ("prior_train_loss", 'b', '-'),
                        ("total_train_loss", 'g', '-'),
@@ -115,4 +148,4 @@ if __name__ == '__main__':
     
     utils.plot_loss_curves(results=results, labels=results_to_plot)
     plt.show()
-    """
+    

utils.py
@@ -3,11 +3,12 @@ import torch
 from matplotlib import pyplot as plt
 
 def softmax_extract_mean(actions_logits: torch.Tensor,
-                         legal_moves: torch.Tensor):
+                         legal_moves: torch.Tensor,
+                         eps=1e-12):
     """
     Applies softmax function to logits. Then extracts the legal moves and normalises the result.
     """
-    priors = actions_logits.softmax(dim=0) # apply softmax
+    priors = actions_logits.softmax(dim=0) + eps # apply softmax. Add eps to prevent divide-by-zero errors
     priors = torch.gather(priors, dim=0, index=legal_moves) # extract just the legal moves
     priors = torch.div(priors, priors.sum()) # renormalise
 