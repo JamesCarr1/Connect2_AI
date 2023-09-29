28fc5d519b439afa72338baafcd856a225baf9a6
Jamie Carr <jamesmatthewcarr@gmail.com>
2023-09-29 18:25:20 +0100

    Continued investigating the performance of the model.

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ def generate_and_train(model,
                        value_optimizer,
                        prior_optimizer,
                        device,
+                       alpha=0.5,
                        schedulers=None):
     """
     Generates a dataset, then trains from it. Saves the updated model and then repeats for num_gens.
@@ -38,17 +39,30 @@ def generate_and_train(model,
         train_dataset, test_dataset, train_dataloader, test_dataloader = prepare_data.prepare_dataloaders(file_path=file_path,
                                                                                                       num_workers=1)
         
+        # # And train
+        # results = engine.train(model=model,
+        #             train_dataloader=train_dataloader,
+        #             test_dataloader=test_dataloader,
+        #             value_optimizer=value_optimizer,
+        #             prior_optimizer=prior_optimizer,
+        #             value_loss_fn=value_loss_fn,
+        #             prior_loss_fn=prior_loss_fn,
+        #             epochs=epochs,
+        #             device=device,
+        #             accuracy_fn=utils.value_acc)
+
         # And train
         results = engine.train(model=model,
                     train_dataloader=train_dataloader,
                     test_dataloader=test_dataloader,
-                    value_optimizer=value_optimizer,
-                    prior_optimizer=prior_optimizer,
+                    optimizer=value_optimizer,
                     value_loss_fn=value_loss_fn,
                     prior_loss_fn=prior_loss_fn,
                     epochs=epochs,
                     device=device,
-                    accuracy_fn=utils.value_acc)
+                    accuracy_fn=utils.value_acc,
+                    alpha=alpha)
+        
         
         # Update model gen and save
         model.gen += 1
@@ -74,9 +88,10 @@ if __name__ == '__main__':
     input_shape = 4
     hidden_units = 16
     output_shape = 1
-    model = model_builder.LinearModelV0(input_shape=input_shape,
+    model = model_builder.ConvModelV0(input_shape=input_shape,
                                         hidden_units=hidden_units,
-                                        output_shape=output_shape).to(device)
+                                        output_shape=output_shape,
+                                        kernel_size=3).to(device)
 
     # Setup loss functions and optimizer
     value_loss_fn = torch.nn.MSELoss()
@@ -91,34 +106,53 @@ if __name__ == '__main__':
     prior_scheduler = lr_scheduler.ExponentialLR(prior_optimizer, gamma=0.99)
 
     
+    # results = generate_and_train(model=model,
+    #                    num_games=1000,
+    #                    num_sims=30,
+    #                    num_gens=100,
+    #                    epochs=1,
+    #                    value_loss_fn=value_loss_fn,
+    #                    prior_loss_fn=prior_loss_fn,
+    #                    value_optimizer=value_optimizer,
+    #                    prior_optimizer=prior_optimizer,
+    #                    device=device,
+    #                    schedulers=(value_scheduler, prior_scheduler))
+
     results = generate_and_train(model=model,
                        num_games=1000,
                        num_sims=10,
                        num_gens=10,
-                       epochs=6,
+                       epochs=1,
                        value_loss_fn=value_loss_fn,
                        prior_loss_fn=prior_loss_fn,
                        value_optimizer=value_optimizer,
                        prior_optimizer=prior_optimizer,
                        device=device,
-                       schedulers=(value_scheduler, prior_scheduler))
+                       schedulers=[value_scheduler],
+                       alpha=0.5)
     
     test_vector = torch.tensor([[1, 0, 0, -1],
                                 [-1, 0, 0, 0],
-                                [0, 0, 1, -1]], dtype=torch.float32).to(device)
+                                [0, 0, 1, -1],
+                                [-1, 0, 0, 1],
+                                [0, -1, 0, 0]], dtype=torch.float32).to(device)
     target_priors = torch.tensor([[0, 0.72979, 0.27020, 0],
                                   [0, 0.41657, 0.25619, 0.327],
-                                  [0.26969, 0.73030, 0, 0]])
-    target_values = torch.tensor([0, -1, 1])
+                                  [0.26969, 0.73030, 0, 0],
+                                  [0, 0.28705, 0.71294, 0],
+                                  [0.27320, 0, 0.33004, 0.396]])
+    target_values = torch.tensor([0, -1, 1, 1, -1])
     
     action_logits, value_logits = model(test_vector)
 
     pred_priors = torch.softmax(action_logits, dim=1)
+
+    log_softmaxed = torch.log_softmax(action_logits, dim=1)
     pred_values = torch.tanh(value_logits)
 
     for i, board_state in enumerate(test_vector):
-        print(f"{board_state} | Priors: {qt(target_priors, i)} vs. {qt(pred_priors, i)} | Values: {round(target_values[i].item(), 4)} vs. {round(pred_values[i].item(), 4)}")
-        #print(f"{board_state} | Priors: {qt(target_priors, i)} vs. {qt(pred_priors, i)} / {qt(action_logits, i)} | Values: {round(target_values[i].item(), 4)} vs. {round(pred_values[i].item(), 4)}")
+        #print(f"{board_state} | Priors: {qt(target_priors, i)} vs. {qt(pred_priors, i)} | Values: {round(target_values[i].item(), 4)} vs. {round(pred_values[i].item(), 4)}")
+        print(f"{board_state} | Priors: {qt(target_priors, i)} vs. {qt(pred_priors, i)} / {qt(action_logits, i)} / {qt(log_softmaxed, i)} | Values: {round(target_values[i].item(), 4)} vs. {round(pred_values[i].item(), 4)}")
 
     
     """
--- model_builder.py
+++ model_builder.py
@@ -5,6 +5,109 @@ import utils
 
 from torch import nn
 
+class ConvModelV0(torch.nn.Module):
+    def __init__(self, input_shape, hidden_units, output_shape, kernel_size):
+        super().__init__()
+
+        # Generation of weights (higher gens have been trained longer)
+        self.gen = 1
+
+        self.input_shape = input_shape
+
+        # Convolutional layer
+        self.conv_layer = nn.Sequential(
+            nn.Conv1d(in_channels=1, out_channels=hidden_units, kernel_size=kernel_size)
+        )
+
+        # Linear layer
+        self.layer_1 = nn.Sequential(
+            nn.Flatten(), # flatten back to a (batch_size, units) (from batch_size, 1, units)
+            nn.Linear(in_features=hidden_units * (5 - kernel_size), out_features=hidden_units),
+            #nn.ReLU(),
+            #nn.Linear(in_features=hidden_units, out_features=hidden_units),
+            #nn.ReLU()
+        )
+
+        # Two types of output: action (i.e choose the best outcome) and value
+        self.action_head = nn.Linear(in_features=hidden_units, out_features=input_shape) # outputs a value for ALL board positions, then mask impossible ones
+        self.value_head = nn.Linear(in_features=hidden_units, out_features=output_shape)
+    
+    def __repr__(self):
+        return "LinearModelV0"
+    
+    def to(self, device):
+        # Updates to to also update self.device
+        super().to(device)
+        self.device = device
+
+        return self
+
+    def forward(self, x):
+        """
+        args:
+            x: current board position
+        returns:
+            action_rating: tensor of length 4, contains a rating for ALL POSSIBLE MOVES
+            value: model rating of current moves
+        """
+        # Need to unsqueeze dim for conv1d
+        # Pass through first layer
+        x = self.layer_1(self.conv_layer(x.unsqueeze(dim=1)))
+
+        # Now pass through specialised heads
+        action_rating = self.action_head(x)
+        value_logits = self.value_head(x) # NOTE THESE ARE NOT NORMALISED TO -1 < y < 1
+        
+        return action_rating, value_logits
+    
+    def predict_priors(self, board_state: list, possible_moves: list) -> list:
+        """
+        Runs the model once, and manipulates the results to return just the priors.
+        args:
+            board_state: the current board state
+            possible_moves: the indexes of all LEGAL moves
+        returns:
+            priors: a list containing the probabilities of choosing each legal move.
+        """
+        # Tensors must be transferred to self.device (as they are generated from lists, so are 'cpu' by default)
+        # Unsqueeze to have shape batch_size, 4
+        x = torch.tensor(board_state, dtype=torch.float32).unsqueeze(dim=0).to(self.device) # convert board state to a tensor
+        possible_moves = torch.tensor(possible_moves, dtype=torch.int64).to(self.device) # possible moves also needs to be converted to a tensors
+
+        # Make a prediction
+        self.eval()
+        with torch.inference_mode():
+            action_ratings, _ = self.forward(x) # run x through the model
+        self.train()
+
+        action_ratings = action_ratings.squeeze() # squeeze back to normal size
+        priors = utils.softmax_extract_mean(actions_logits=action_ratings,
+                                            legal_moves=possible_moves)
+
+        return priors.tolist() # return just the values
+
+    def predict_value(self, board_state: list) -> float:
+        """
+        Runs the model once, and manupulates the results to return just the values.
+        args:
+            board_state: the current board state
+        returns:
+            value: value from -1 to 1 predicting the result.
+        """
+        # Tensor must be transferred to self.device (as they are generated from lists, so are 'cpu' by default)
+        x = torch.tensor(board_state, dtype=torch.float32).unsqueeze(dim=0).to(self.device) # convert to tensor
+
+        # Make a prediction
+        self.eval()
+        with torch.inference_mode():
+            _, value_logits = self.forward(x) # value needs to be normalised to -1 to 1. tanh is perfect for this
+        self.train()
+
+        value_logits = value_logits.squeeze() # squeeze back to normal size
+        value = torch.tanh(value_logits) # converts to -1 to 1
+
+        return value.item() # returns just the float
+
 class LinearModelV0(torch.nn.Module):
     def __init__(self, input_shape, hidden_units, output_shape):
         super().__init__()
@@ -43,6 +146,7 @@ class LinearModelV0(torch.nn.Module):
             value: model rating of current moves
         """
         # Pass through first layer
+        print(x.shape)
         x = self.layer_1(x)
 
         # Now pass through specialised heads
